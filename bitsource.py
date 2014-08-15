513f464074acae876df90782e574c4544bdd4dbe
barisser <barisser@gmail.com>
2014-08-15 14:32:36 -0700

    flaskfixes

--- bitsource.py
+++ bitsource.py
@@ -8,8 +8,8 @@ import node
 
 node_url=node.url
 #node_port=node.port
-username='barisser'
-password='2bf763d2132a2ccf3ea38077f79196ebd600f4a29aa3b1afd96feec2e7d80beb3d9e13d02d56de0f'
+username="barisser"
+password="2bf763d2132a2ccf3ea38077f79196ebd600f4a29aa3b1afd96feec2e7d80beb3d9e13d02d56de0f"
 
 def connect(command,params):
   global url, headers, payload
@@ -17,7 +17,6 @@ def connect(command,params):
   headers={"content-type":"application/json"}
   payload=json.dumps({"method":command,"params":params})
   response=requests.get(url,headers=headers,data=payload)
-
   response=json.loads(response.content)
   return response['result']
 
