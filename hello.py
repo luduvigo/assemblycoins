86c291a56d7c003dafea4b56696285cb4ef712c7
barisser <barisser@gmail.com>
2014-08-15 12:00:02 -0700

    flaskfixes

--- hello.py
+++ hello.py
@@ -206,4 +206,5 @@ def checkaddresses():  #FOR PAYMENT DUE      #WORKS
 
 if __name__ == '__main__':
     #app.run(host= '0.0.0.0', debug=False)
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
