ceab4c5764facd8aa48361a68190c2703c147aae
barisser <barisser@gmail.com>
2014-08-15 13:10:36 -0700

    flaskfixes

--- hello.py
+++ hello.py
@@ -88,11 +88,12 @@ def transfer_transaction_serverside():
 
 @app.route('/transactions/<transaction_hash>')
 def getrawtransaction(transaction_hash=None):
-  response=bitsource.tx_lookup(str(transaction_hash))
+  #response=bitsource.tx_lookup(str(transaction_hash))
   #print response
   #response=make_response(str(response), 200)
   #response.headers['Access-Control-Allow-Origin']= '*'
-  return str(response)
+  #return str(response)
+  return str(transaction_hash)
 
 @app.route('/colors/statements/<address>')     #WORKS
 def readmultistatements(address=None):
