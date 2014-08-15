ed23de2e47bc64e45d2a29af484ce6cfd92e20d9
barisser <barisser@gmail.com>
2014-08-15 14:21:03 -0700

    flaskfixes

--- hello.py
+++ hello.py
@@ -35,7 +35,7 @@ def something():
 
 @app.route('/blocks/count')
 def getblockcount():
-  count=bitsource.connect('getblockcount',[])
+  #count=bitsource.connect('getblockcount',[])
   response=make_response("aa", 200)
   response.headers['Access-Control-Allow-Origin']= '*'
   return response
