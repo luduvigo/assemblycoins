17d70f071016d4a8eac236eab14b9f0d5d468994
barisser <barisser@gmail.com>
2014-08-15 12:35:39 -0700

    flaskfixes

--- node.py
+++ node.py
@@ -3,13 +3,13 @@ import json
 
 
 #node_url= '199.188.192.144' #'127.0.0.1'#'71.198.63.116'##
-node_url='127.0.0.1'
-node_port='8332'
+url='43f6400d.ngrok.com'
+#node_port='8332'
 username='barisser'
 password='2bf763d2132a2ccf3ea38077f79196ebd600f4a29aa3b1afd96feec2e7d80beb3d9e13d02d56de0f'
 
 def connect(command,params):
-  url='http://'+username+':'+password+'@'+node_url+':'+node_port
+  url='http://'+username+':'+password+'@'+url#+':'+node_port
   headers={'content-type':'application/json'}
   payload=json.dumps({'method':command,'params':params})
   response=requests.get(url,headers=headers,data=payload)
