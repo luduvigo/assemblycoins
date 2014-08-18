95693229d8afc690ebf0e51053cc273e99c37504
barisser <barisser@gmail.com>
2014-08-18 14:22:27 -0700

    redis

--- worker.py
+++ worker.py
@@ -6,7 +6,7 @@ from hello import workerstuff
 
 listen =['high', 'default', 'low']
 
-redis_url= os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+redis_url= os.getenv('REDISCLOUD_URL', 'redis://localhost:6379')
 
 conn=redis.from_url(redis_url)
 
