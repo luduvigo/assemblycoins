ce5034090b0c0694a858ac8ab54c50bc12fb2af6
barisser <barisser@gmail.com>
2014-08-18 14:52:09 -0700

    redis

--- worker.py
+++ worker.py
@@ -6,13 +6,16 @@ from hello import workerstuff
 
 listen =['high', 'default', 'low']
 
-redis_url= os.getenv('REDISCLOUD_URL', 'redis://localhost:6379')
+redis_url= urlparse.urlparse(os.environ.get('REDISCLOUD_URL'))
+#r=redis.Redis(host=url.hostname, port=url.port, password=url.password)
+
+redis_url = os.getenv('REDISCLOUD_URL', 'redis://localhost:6379')
 print redis_url
 conn=redis.from_url(redis_url)
 
 
 
-q=Queue(connection=workerstuff)
+q=Queue(connection=conn)
 result=q.enqueue(workerstuff)
 
 if __name__ == '__main__':
