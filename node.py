ffd3e7fdd2169f57eac254c432a9e5129e349445
barisser <barisser@gmail.com>
2014-08-19 15:05:59 -0700

    node

--- node.py
+++ node.py
@@ -4,14 +4,15 @@ import os
 from requests.auth import HTTPBasicAuth
 
 #node_url= '199.188.192.144' #'127.0.0.1'#'71.198.63.116'##
-url="blockway.asm.co"
+#url="blockway.asm.co"
+url='http://50a47aa8.ngrok.com'
 #node_port='8332'
 #username='u'
 
-#username="barisser"
-username=os.environ['node_username']
-#password="2bf763d2132a2ccf3ea38077f79196ebd600f4a29aa3b1afd96feec2e7d80beb3d9e13d02d56de0f"#
-password=os.environ['node_password']
+username="barisser"
+#username=os.environ['node_username']
+password="2bf763d2132a2ccf3ea38077f79196ebd600f4a29aa3b1afd96feec2e7d80beb3d9e13d02d56de0f"#
+#password=os.environ['node_password']
 
 def connect(command,params):
   connect_url='https://'+url#+':'+node_port
