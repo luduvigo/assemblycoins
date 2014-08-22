87c2ec6f7dc4d60543ba98bdc1f8bcc553741022
barisser <barisser@gmail.com>
2014-08-22 15:56:57 -0700

    ko

--- hello.py
+++ hello.py
@@ -222,7 +222,7 @@ def color_txs_in_block(blockn=None):
 
 @app.route('/color/new', methods=['POST'])
 def newcolor():
-  fromaddr=str(request.form['fromaddr'])
+  fromaddr=request.form['fromaddr']
   # colornumber=str(request.form['colornumber'])
   # colorname=str(request.form['colorname'])
   # destination=str(request.form['destination'])
