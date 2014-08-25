3984376a2629aca4e71a6fc1e052ad6a4e529d20
barisser <barisser@gmail.com>
2014-08-24 18:21:01 -0700

    db

--- hello.py
+++ hello.py
@@ -184,9 +184,9 @@ def givenewaddress():
   color_address=addresses.hashlib.sha256(coin_name).hexdigest() #FIGURE THIS OUT
 
   # #write address to db
-   amount_expected=str(int(tosend*100000000))
-   amount_received="0"
-   amount_withdrawn="0"
+  amount_expected=str(int(tosend*100000000))
+  amount_received="0"
+  amount_withdrawn="0"
   # k=databases.add_address(public_address, private_key, amount_expected, amount_received, amount_withdrawn, coin_name, color_amount, dest_address, description, ticker)
   # print k
 
