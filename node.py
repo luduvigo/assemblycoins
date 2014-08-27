db17c7aa9371265ea3a279ef242e18ec9fc25f31
barisser <barisser@gmail.com>
2014-08-27 10:39:46 -0700

    url

--- node.py
+++ node.py
@@ -3,10 +3,10 @@ import json
 import os
 from requests.auth import HTTPBasicAuth
 
-url="blockway.asm.co"
+#url="blockway.asm.co"
 #url='385a0239.ngrok.com'
 #node_port='8332'
-
+url=os.environ['node_url']
 username=os.environ['node_username']
 password=os.environ['node_password']
 
