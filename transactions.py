c823a283276441508a8adb475ebdf2cafaaed665
barisser <barisser@gmail.com>
2014-09-06 13:47:23 -0700

    transfer tweaks

--- transactions.py
+++ transactions.py
@@ -351,6 +351,7 @@ def find_transfer_inputs(fromaddr, coloraddress, coloramt, btc):
   return answer, totalfound
 
 def transfer_tx(fromaddr, dest, fee, privatekey, sourceaddress, coloramt, othermeta):
+  global inputdata
   btcneeded=fee+dust*4
   coloraddress=databases.first_coloraddress_from_sourceaddress(sourceaddress)
   result=''
