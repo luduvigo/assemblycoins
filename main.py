89571d52785d4a285c55f784cdb46fbdb833bfec
barisser <barisser@gmail.com>
2014-09-09 16:12:28 -0700

    api

--- main.py
+++ main.py
@@ -145,7 +145,7 @@ def givenewaddress_specifics():
   #ticker=request.form['ticker']
   #email=request.form['email']
 
-  fee_each=request.form['fee_each']
+  fee_each=float(request.form['fee_each'])
   markup=1
   tosend=str(transactions.creation_cost(color_amount, coin_name, "", description, fee_each, markup))
 
