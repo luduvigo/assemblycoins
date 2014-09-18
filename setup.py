908d6dbe0c731c6e25e082db55903deb9bccd5f7
barisser <barisser@gmail.com>
2014-09-18 11:11:21 -0700

    pipified

--- setup.py
+++ setup.py
@@ -1,2 +1,10 @@
-import os
 from setuptools import setup
+
+setup(name='Assets',
+      version='0.1',
+      author='Andrew Barisser',
+      author_email='barisser@gmail.com',
+      license='MIT',
+      description='Digital Tokens on the Bitcoin Blockchain',
+      packages=['assets']
+    )
