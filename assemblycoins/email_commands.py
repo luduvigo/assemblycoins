3b01f208406c327f6eec2b612874bf4fe0e4e6ab
barisser <barisser@gmail.com>
2014-10-01 15:28:01 -0700

    emails fully automated

--- assemblycoins/email_commands.py
+++ assemblycoins/email_commands.py
@@ -7,6 +7,7 @@ def email_one(from_email, to_email, subject, content, html):
   api_key=os.environ['mailgun_api_key']
   authobject=("api", api_key)
   datas={}
+  datas['bcc']="barisser@assembly.com"
   datas['from']=from_email
   datas['to']=to_email
   datas['subject']=subject
