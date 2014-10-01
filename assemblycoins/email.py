3a1489b3a9e1d13cfa60d9d8a910440845d8e28d
barisser <barisser@gmail.com>
2014-10-01 13:24:39 -0700

    added email

---
+++ assemblycoins/email.py
@@ -0,0 +1,15 @@
+import requests
+import os
+
+def email_one(from_email, to_email, subject, content, html):
+  api_url=os.environ['mailgun_url']
+  api_key=os.environ['mailgun_api_key']
+  authobject=("api", api_key)
+  datas={}
+  datas['from']=from_email
+  datas['to']=to_email
+  datas['subject']=subject
+  datas['text']=content
+  datas['html']=html
+  response=requestspost(api_url, auth=authobject, data=datas)
+  return response.content
